58556fd175551d6abdf4e8557b6b0af7d8e2a074
2400172 <116487866+Miru1218@users.noreply.github.com>
2024-08-27 15:37:36 +0800

    添加effect方法和onCleanup做法

--- projects/project/src/app/pagination-signal/pagination-signal.component.ts
+++ projects/project/src/app/pagination-signal/pagination-signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, signal, computed } from '@angular/core';
+import { Component, Input, Output, EventEmitter, signal, computed, effect } from '@angular/core';
 
 @Component({
   selector: 'app-pagination-signal',
@@ -10,45 +10,62 @@ export class PaginationSignalComponent {
   @Input() pageCount = 1; // 總頁數，預設為 1
   @Output() pageChange = new EventEmitter<number>(); // 當頁碼變更時，發出頁碼的事件
 
-  currentPage = signal(1);
+  currentPage = signal(this.page); // 用於記錄當前頁碼的信號，初始值為輸入的頁碼
 
   previousPage = computed(() => {
-    // 檢查頁碼是否大於 1
-    return this.currentPage() > 1
-      ? this.currentPage() - 1
-      : this.currentPage();
-  })
+    // 計算前一頁的頁碼，如果當前頁碼大於 1，則減一；否則返回當前頁碼
+    return this.currentPage() > 1 ? this.currentPage() - 1 : this.currentPage();
+  });
 
   nextPage = computed(() => {
-    // 檢查頁碼是否小於總頁數
-    return this.currentPage() < this.pageCount
-      ? this.currentPage() + 1
-      : this.currentPage()
+    // 計算下一頁的頁碼，如果當前頁碼小於總頁數，則加一；否則返回當前頁碼
+    return this.currentPage() < this.pageCount ? this.currentPage() + 1 : this.currentPage();
   });
 
   ngOnInit() {
+    // 在組件初始化時，將 currentPage 設置為輸入的頁碼
     this.currentPage.set(this.page);
   }
 
   jumpTo(num: number) {
-    // 若頁碼不在合理範圍內，則不執行跳轉
+    // 若目標頁碼不在合理範圍內（小於1或大於總頁數），則不執行跳轉
     if (num < 1 || num > this.pageCount) {
       return;
     }
 
-    this.currentPage.set(num); // 設定當前頁碼
-
-    // 發出頁碼變更事件，將新的頁碼傳遞出去
+    // 設定當前頁碼為目標頁碼，並發出頁碼變更事件
+    this.currentPage.set(num);
     this.pageChange.emit(num);
-
   }
 
   goPrevious() {
-    this.jumpTo(this.currentPage() - 1); // 跳轉到前一頁
+    // 跳轉到前一頁
+    this.jumpTo(this.previousPage());
   }
 
   goNext() {
-    this.jumpTo(this.currentPage() + 1); // 跳轉到下一頁
+    // 跳轉到下一頁
+    this.jumpTo(this.nextPage());
   }
 
+  private logTodoResponse = effect((onCleanup) => {
+    // 將當前頁碼存入 localStorage，並輸出到控制台
+    localStorage.setItem('pageNumber', this.currentPage().toString());
+    console.log(this.currentPage());
+
+    // 設置一個定時器，在 1 秒後輸出當前頁碼
+    const timer = setTimeout(() => {
+      console.log('1 second ago, the user was on page', this.currentPage().toString());
+    }, 1000);
+
+    // 使用 onCleanup 來確保定時器在 effect 被銷毀時清理掉
+    onCleanup(() => {
+      clearTimeout(timer);
+    });
+  });
+
+  ngOnDestroy(): void {
+    // 在組件銷毀時，手動銷毀 effect，以防止內存泄漏
+    this.logTodoResponse.destroy();
+  }
 }
